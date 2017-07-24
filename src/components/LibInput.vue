08b87b44375034b69123469d5f9eaf18258ca4ab
e-e-e <e-e-e@users.noreply.github.com>
2017-07-24 23:17:18 +1000

    starts of an input field

---
+++ src/components/LibInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <label :for="name"><div v-if="icon" :class="['fa', iconName]"></div></label>
+    <input :name="name" :type="type" />
+  </div>
+</template>
+
+<script>
+  import { getStatusStyle, getSizeStyle } from '../utils/styleHelpers.js';
+
+  export default {
+    name: 'LibInput',
+    components: {},
+    props: {
+      type: {
+        type: String,
+        default: 'text',
+      },
+      name: {
+        type: String,
+        default: 'text',
+      },
+      icon: {
+        type: String,
+      },
+      status: {
+        type: String,
+        default: '',
+      },
+      size: {
+        type: String,
+        default: '',
+      },
+    },
+    data() {
+      return {};
+    },
+    computed: {
+      iconName() { return getIconName(this.icon); },
+      statusStyle() { return getStatusStyle(this.status); },
+      sizeStyle() { return getSizeStyle(this.size); },
+    },
+    methods: {},
+};
+</script>
